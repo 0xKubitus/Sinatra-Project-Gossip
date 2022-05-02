d1c62323e5fb3f60ffdc3c95b430771baacf5dda
0xKubitus <n.cubilier@gmail.com>
2022-05-02 12:22:55 +0200

    2.2.2.e = OK

--- lib/controller.rb
+++ lib/controller.rb
@@ -9,6 +9,12 @@ class ApplicationController < Sinatra::Base
     erb :new_gossip
   end
 
+
+  # paramétrer la route POST en sortie pour la création de nouveau gossip :
+  post '/gossips/new/' do
+    puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+  end
+
   #run! if app_file == $0 # <= exécuter le code de la classe sans avoir à écrire un ApplicationController.new.perform en bas du fichier (inutile ici car on va utiliser config.ru)
 
 end #fin de la classe ApplicationController
