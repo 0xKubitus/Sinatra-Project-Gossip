1b13d2bd0bd17f27771ff0998206bf08737a81ee
0xKubitus <n.cubilier@gmail.com>
2022-05-02 12:06:18 +0200

    2.2.2.a = OK

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < Sinatra::Base
+
   get '/' do
     erb :index # <= aller chercher le fichier index.erb rangé dans le dossier /views
   end
 
+
+  get'/gossips/new/' do
+    erb :new_gossip
+  end
+
   #run! if app_file == $0 # <= exécuter le code de la classe sans avoir à écrire un ApplicationController.new.perform en bas du fichier (inutile ici car on va utiliser config.ru)
-end
+
+end #fin de la classe ApplicationController
