e65fd76c4d902698bb9db619ba8faf51eaed6bf4
0xKubitus <n.cubilier@gmail.com>
2022-05-02 11:32:19 +0200

    ajout view index.erb

--- controller.rb
+++ controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < Sinatra::Base
   get '/' do
-    "<html>
-      <head>
-        <title>The Gossip Project</title>
-      </head>
-      <body>
-        <h1>Le site des ragots du coin !</h1>
-      </body>
-    </html>"
+    erb :index # <= aller chercher le fichier index.erb rangé dans le dossier /views
   end
 
   #run! if app_file == $0 # <= exécuter le code de la classe sans avoir à écrire un ApplicationController.new.perform en bas du fichier (inutile ici car on va utiliser config.ru)
