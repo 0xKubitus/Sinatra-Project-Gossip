000478b1006593935797f5d1a207cef9f2d69453
0xKubitus <n.cubilier@gmail.com>
2022-05-02 15:14:55 +0200

    2.4.2 OK

---
+++ README.md
@@ -0,0 +1,3 @@
+THP - Projet du Lundi 2 Mai 
+
+https://www.thehackingproject.org/fr/dashboard/courses/1/weeks/2/days/1
--- lib/controller.rb
+++ lib/controller.rb
@@ -15,7 +15,12 @@ class ApplicationController < Sinatra::Base
   # paramétrer la route POST en sortie pour la création de nouveau gossip :
   post '/gossips/new/' do
     puts "Si ce programme fonctionne, tu devrais trouver un gossip dans le fichier 'gossip.csv' "
-    Gossip.new(les_entrées_du_gossip).save
+    puts "Salut, je suis dans le serveur"
+    puts "Ceci est le contenu du hash params : #{params}"
+    puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+    puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+    puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
   end
 
 
@@ -33,4 +38,3 @@ class ApplicationController < Sinatra::Base
 
 end #fin de la classe ApplicationController
 
-Gossip.new("Kubi", "ta soeur la chauve").save
